ba414bc76975bf51e816ceabe63974d308d7f7a9
baileybunch5dad <baileybunch5dad@gmail.com>
2024-12-28 21:35:51 +0000

    try different methods of returning values from async routines

--- aiopost.py
+++ aiopost.py
@@ -1,11 +1,29 @@
 import aiohttp
 import asyncio
 
-async def post_request():
+# https://scrapeops.io/python-web-scraping-playbook/python-aiohttp-post-requests/
+
+# import aiohttp
+# import asyncio
+
+# async def post_request():
+#     async with aiohttp.ClientSession() as session:
+#         response = await session.post(url="https://httpbin.org/post",
+#                                       data={"key": "value"},
+#                                       headers={"Content-Type": "application/json"})
+#         print(await response.json())
+
+# asyncio.run(post_request())
+
+# my modified version
+
+async def post_request() -> str:
     async with aiohttp.ClientSession() as session:
         response = await session.post(url="https://httpbin.org/post",
                                       data={"key": "value"},
                                       headers={"Content-Type": "application/json"})
-        print(await response.json())
+        s = await response.json()
+        return s
 
-asyncio.run(post_request())
+r = asyncio.run(post_request())
+print(r)
