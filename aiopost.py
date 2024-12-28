d0b9c3fda4d6a2f2e2507d18a277e133505557aa
baileybunch5dad <baileybunch5dad@gmail.com>
2024-12-28 21:27:54 +0000

    use aio with web requests

---
+++ aiopost.py
@@ -0,0 +1,11 @@
+import aiohttp
+import asyncio
+
+async def post_request():
+    async with aiohttp.ClientSession() as session:
+        response = await session.post(url="https://httpbin.org/post",
+                                      data={"key": "value"},
+                                      headers={"Content-Type": "application/json"})
+        print(await response.json())
+
+asyncio.run(post_request())
