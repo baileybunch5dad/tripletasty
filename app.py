849a29dd1eb067b7fdb547eae0188f4490e1bbee
baileybunch5dad <baileybunch5dad@gmail.com>
2025-01-01 18:35:14 +0000

    finally got weibull distribution working

--- app.py
+++ app.py
@@ -24,15 +24,23 @@ def home():
     print(values)
 
     print("weibull shape 5.")
-    values = [np.mean(np.random.weibull(5., 1000)) for i in range(200)]
-    values = [int(v * 100) for v in values]
-    # print(values)
-    labels = [str(i) for i in range(len(values))]
-    # print(labels)
-    # print("render")
-    xaxis = [z for z in range(10)]
-    yaxis = [x-5 for x in range(10)]
-    # passdata = { 'x': xaxis, 'y': yaxis }
+    values = [np.mean(np.random.weibull(5., 10000)) for i in range(10000)]
+    # values = [int(v * 100) for v in values]
+    # # print(values)
+    # labels = [str(i) for i in range(len(values))]
+    # # print(labels)
+    # # print("render")
+    # xaxis = [z for z in range(10)]
+    # yaxis = [x-5 for x in range(10)]
+    # # passdata = { 'x': xaxis, 'y': yaxis }
+    # yaxis = [int(v) for v in values]
+    # xaxis = [x for x in range(len(values))]
+
+    hist, bin_edges = np.histogram(values, bins='auto')
+    xaxis = [float(x) for x in bin_edges]
+    yaxis = [int(y) for y in hist]
+    print(hist)
+    print(bin_edges)
 
     # return render_template("graph.html", labels=labels, values=values)
     return render_template("graph3.html", xlist=xaxis, ylist=yaxis)
